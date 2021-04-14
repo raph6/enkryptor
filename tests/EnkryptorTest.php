e6eb38fed348cb19f23270573bf91de685807243
Raph <rbleuzet@gmail.com>
2021-04-14 02:35:59 +0200

    new unit tests

--- tests/EnkryptorTest.php
+++ tests/EnkryptorTest.php
@@ -6,43 +6,59 @@ use PHPUnit\Framework\TestCase;
 
 class EnkryptorTest extends TestCase {
 
+    private $_str = 'foo';
+    private $_password = 'bar';
+    private $_defaultEncrypted = 'oYhrZJRMLU1s28EQvChbAlFaeGNhd1VjSHVkcGF0ZERTQ201TXc9PQ==';
+    private $_ciphersAndEncrypted = [
+        'chacha20-poly1305' => 'xMGUVId4M0M3kHaIQXNxNg==',
+        'bf-cbc' => '5h1Rm8w+5vQ1NStiRi9HUzgrYz0=',
+        'seed-cbc' => 'BfBRX49ZH9asPxDsxLqRXFdIM1ViSS9sa2IrR0RtN2hObmJRWkE9PQ==',
+        'des-ede3-cfb1' => 'DF+HcRPTHn9XYUc3'
+    ];
+
     public function testCanBeDecrypted()
     {
-        $encrypted = 'KhB5bxPJ2sQ7kSlBMAV7w0dtUVkyUklLU1FHdXY5eTNiNnlicHc9PQ==';
-        $password = 'test decrypt';
-        $str = 'test';
         $this->assertEquals(
-            $str,
-            Enkryptor::decrypt($encrypted, $password)
+            $this->_str,
+            Enkryptor::decrypt($this->_defaultEncrypted, $this->_password)
         );
+
+        foreach ($this->_ciphersAndEncrypted as $cipher => $encryptedString) {
+            $this->assertEquals(
+                $this->_str,
+                Enkryptor::decrypt($encryptedString, $this->_password, $cipher)
+            );
+        }
+    }
+
+    public function testCanBeEncrypted()
+    {
+        $this->assertIsString(Enkryptor::encrypt($this->_str, $this->_password));
+
+        foreach ($this->_ciphersAndEncrypted as $cipher => $encryptedString) {
+            $this->assertIsString(Enkryptor::encrypt($this->_str, $this->_password, $cipher));
+        }
     }
 
     public function testEncryptDecrypt()
     {
-
-        $str = 'test unitaire 2';
-        $password = '77&$#@&';
-        $encrypted = Enkryptor::encrypt($str, $password);
+        $encrypted = Enkryptor::encrypt($this->_str, $this->_password);
         $this->assertEquals(
-            $str,
-            Enkryptor::decrypt($encrypted, $password)
+            $this->_str,
+            Enkryptor::decrypt($encrypted, $this->_password)
         );
 
-        $cipher = 'des-ede3-cfb1';
-        $encryptedcipher = Enkryptor::encrypt($str, $password, $cipher);
-        $this->assertEquals(
-            $str,
-            Enkryptor::decrypt($encryptedcipher, $password, $cipher)
-        );
-
-        $this->assertEquals(
-            '',
-            Enkryptor::decrypt(Enkryptor::encrypt('', ''), '')
-        );
+        foreach ($this->_ciphersAndEncrypted as $cipher => $encryptedString) {
+            $encryptedWithCipher = Enkryptor::encrypt($this->_str, $this->_password, $cipher);
+            $this->assertEquals(
+                $this->_str,
+                Enkryptor::decrypt($encryptedWithCipher, $this->_password, $cipher)
+            );
+        }
     }
 
     public function testCannotBeDecrypted() {
-        $encrypted = 'c2+foLFa9OGfOjjsJ/n8m0NmcjJ6MWYzVzgyeENBcDRZSFY0NGc9PQ==';
+        $encrypted = 'RIbkxq90BhULlZavRmVa73lSWC9SNVpHN0d6R1Z5cFAvWm9IVFJraEpKQmhMTnRkSFZRU0JXOG9EQ2c9';
         $password = 'haha';
         $this->assertFalse(Enkryptor::decrypt(null, $password));
         $this->assertFalse(Enkryptor::decrypt($encrypted, null));
